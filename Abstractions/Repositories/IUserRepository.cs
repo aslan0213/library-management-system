15abd026c8f9c5d750a9f8c2fbe35f2ebcad2e3d
aslan0213 <aslanmamedov0213@gmail.com>
2026-07-31 03:27:23 +0400

    feat: add auth-related repository and service interfaces, AuthResult and TokenResult

---
+++ Abstractions/Repositories/IUserRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Domain.Entities;
+
+namespace Abstractions.Repositories
+{
+	public interface IUserRepository : IRepositoryBase<User>
+	{
+		Task<User?> GetByEmailAsync(string email, CancellationToken cancellationToken = default);
+	}
+}
---
+++ Abstractions/Services/IJwtTokenGenerator.cs
@@ -0,0 +1,13 @@
+﻿using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Abstractions.Services
+{
+	public interface IJwtTokenGenerator
+	{
+		(string token, DateTime ExpiredAt) GenerateAccessToken(User user);	
+		string GenerateRefreshToken();
+	}
+}
