2ebd2379e1e9223cc3a3994bef3c8ae9c99e06f3
aslan0213 <aslanmamedov0213@gmail.com>
2026-07-30 15:31:20 +0400

    feat: add User, RefreshToken, Role entities and auth-related domain exceptions

---
+++ Domain/Entities/User.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Domain.Entities
+{
+	public class User : IHasId
+	{
+		public Guid Id { get; set; }
+		public string Email { get; set; } = string.Empty;
+		public string PasswordHash { get; set; } = string.Empty;
+		public Role role { get; set; } = Role.User;
+		public DateTime CreatedAt { get; set; } 
+		public ICollection<RefreshToken> RefreshTokens { get; set; } = new List<RefreshToken>();
+
+
+	}
+}
---
+++ Domain/Exceptions/InvalidCredentialException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Domain.Exceptions
+{
+	public class InvalidCredentialException : Exception
+	{
+		public InvalidCredentialException():base("Invalid email or password.")
+		{
+
+		}
+	}
+}
