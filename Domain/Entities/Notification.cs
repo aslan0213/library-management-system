853710f0249f79ea5b2b91aeb372264887fe627c
aslan0213 <aslanmamedov0213@gmail.com>
2026-08-09 02:43:48 +0400

    feat: add Publisher, Notification, Category, Reservation entities and update Book relationships

---
+++ Domain/Entities/Notification.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Domain.Entities
+{
+	public class Notification : IHasId
+	{
+		public Guid Id { get; set; }
+		public string Message { get; set; } = string.Empty;
+		public Guid MemberId { get; set; }
+		public Member Member { get; set; } = null!;
+		public DateTime SentAt { get; set; }
+		public bool IsRead { get; set; }
+	}
+}
---
+++ Domain/Entities/Reservation.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Domain.Entities
+{
+	public enum ReservationStatus
+	{
+		Pending,
+		Fullfilled,
+		Canceled
+	}
+	public class Reservation : IHasId
+	{
+		public Guid Id { get; set; }
+		public Guid BookId { get; set; }
+		public Book Book { get; set; } = null!;
+		public Guid MemberId { get; set; }
+		public Member Member { get; set; } = null!;
+		public DateTime ReservedAt { get; set; }
+		public ReservationStatus Status { get; set; } = ReservationStatus.Pending;
+	}
+}
