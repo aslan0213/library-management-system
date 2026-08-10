ab12d4b2ce9db620f92f568e8fff7113d3eae114
aslan0213 <aslanmamedov0213@gmail.com>
2026-08-10 20:01:39 +0400

    feat: add service interfaces for Category, Notification, Publisher

---
+++ Abstractions/Services/ICategoryService.cs
@@ -0,0 +1,17 @@
+﻿using Abstractions.Paging;
+using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Abstractions.Services
+{
+	public interface ICategoryService
+	{
+		Task<Category?> GetByIdAsync(string categoryId, CancellationToken cancellationToken = default);
+		Task<PagedResult<Category>> GetPagedAsync(PagedQuery query, CancellationToken cancellationToken = default);
+		Task<Category> CreateAsync(Category category, CancellationToken cancellationToken = default);
+		Task UpdateAsync(Category category, CancellationToken cancellationToken = default);
+		Task DeleteAsync(Guid categoryId, CancellationToken cancellationToken = default);
+	}
+}
---
+++ Abstractions/Services/IPublisherService.cs
@@ -0,0 +1,17 @@
+﻿using Abstractions.Paging;
+using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Abstractions.Services
+{
+	public interface IPublisherService
+	{
+		Task<Publisher?> GetByIdAsync(Guid id, CancellationToken cancellationToken = default);
+		Task<PagedResult<Publisher>> GetPagedAsync(PagedQuery query, CancellationToken cancellationToken = default);\
+		Task<Publisher> CreateAsync(Publisher publisher, CancellationToken cancellationToken = default);
+		Task UpdateAsync(Publisher publisher, CancellationToken cancellationToken = default);
+		Task DeleteAsync(Guid id, CancellationToken cancellationToken = default);
+	}
+}
