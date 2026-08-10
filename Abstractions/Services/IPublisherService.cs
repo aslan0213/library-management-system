718de71c33f59445840428300a311364690698b1
aslan0213 <aslanmamedov0213@gmail.com>
2026-08-11 03:23:55 +0400

    fix: minor change

--- Abstractions/Services/IPublisherService.cs
+++ Abstractions/Services/IPublisherService.cs
@@ -9,7 +9,7 @@ namespace Abstractions.Services
 	public interface IPublisherService
 	{
 		Task<Publisher?> GetByIdAsync(Guid id, CancellationToken cancellationToken = default);
-		Task<PagedResult<Publisher>> GetPagedAsync(PagedQuery query, CancellationToken cancellationToken = default);\
+		Task<PagedResult<Publisher>> GetPagedAsync(PagedQuery query, CancellationToken cancellationToken = default);
 		Task<Publisher> CreateAsync(Publisher publisher, CancellationToken cancellationToken = default);
 		Task UpdateAsync(Publisher publisher, CancellationToken cancellationToken = default);
 		Task DeleteAsync(Guid id, CancellationToken cancellationToken = default);
